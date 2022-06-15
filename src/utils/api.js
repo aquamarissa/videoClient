0db1b5c00d32da4392d27d608673315461a57c72
aquamarissa <vremapriklucenij16@gmail.com>
2022-06-15 22:00:07 +0300

    finita la comedia

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const serverApi = "http://localhost:5002/api";
-const serverApi = "https://video-server-rose.vercel.app/api";
+const serverApi = "https://04fa-37-73-103-44.eu.ngrok.io/api";
 export const getRoomExists = async (roomId) => {
   const response = await axios.get(`${serverApi}/room-exists/${roomId}`);
   return response.data;
